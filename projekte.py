0d97fed663ee98c324552228b3adf911a49fbb26
Gerrit Kocherscheidt <gerrit.kocherscheidt@kocoautomotive.de>
2022-06-13 08:11:51 +0200

    Projektstruktur umgestellt und projekte.py als Einstiegspunkt ergänzt

---
+++ projekte.py
@@ -0,0 +1,7 @@
+from motorcalc.motorcalc import CDCMotor
+
+if __name__=="__main__":
+    """
+    Hier werden alle Projekte eingehängt, und diese Datei wird für die Berechnung ausgeführt
+    """
+    pass
