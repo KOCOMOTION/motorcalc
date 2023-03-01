19fed012edcc9a646a0c1c68539531636cd8eb72
Gerrit Kocherscheidt <kochersg@koco-group.com>
2023-03-01 08:24:35 +0100

    requirements.txt ergänzt.

--- projekte.py
+++ projekte.py
@@ -1,7 +1,10 @@
 from motorcalc.motorcalc import CDCMotor
 
+def calc_motor_schulung():
+    pass
+
 if __name__=="__main__":
     """
     Hier werden alle Projekte eingehängt, und diese Datei wird für die Berechnung ausgeführt
     """
-    pass
+    calc_motor_schulung()
